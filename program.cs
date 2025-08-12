d526cd353244c622869ab95d7ff2366c8601f152
TshekoO <tshekoogotlhe101@gmail.com>
2025-08-12 10:46:03 +0200

    class expense

--- program.cs
+++ program.cs
@@ -7,5 +7,19 @@ using System.Text.Json;
 
 namespace ExpenseTracker
 {
-    
+    //represents a single expense record 
+
+    public class Expense
+    {
+        public decimal Amount { get; set; } // Money spent
+        public string Category { get; set; }// Expense category 
+        public string Description { get; set; } // Optional note
+        public DateTime Date { get; set; } // Date expense occurred
+
+        // Override ToString() to display expenses in a nice format
+        public override string ToString()
+        {
+            return $"{Date:yyyy-MM-dd} | {Category,-15} | R{Amount,-8} | {Description}";
+        }
+    }
 }
